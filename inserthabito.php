78813ffd04286f16d7b42cd4a53ef3744d20d814
Kali <131703095+KaliRibeiro@users.noreply.github.com>
2024-12-12 17:29:01 -0300

    Update inserthabito.php

--- inserthabito.php
+++ inserthabito.php
@@ -5,8 +5,7 @@ $senha = "";
 $bancodedados = "listadehabito";
 
 
-// Abre a conexão com o banco
-// de dados listadehabito
+// Abre a conexão com o banco de dados listadehabito
 
 $conexao = new mysqli( $servidor
  , $usuario
@@ -14,41 +13,36 @@ $conexao = new mysqli( $servidor
  , $bancodedados);
 
 
-// Verifica se houve erro ao
-// abrir a conexão
+// Verifica se houve erro ao abrir a conexão
 
 
 if ($conexao->connect_error) {
  die("A conexão falhou: " . $conexao->connect_error);
 }
 // Busca nome que foi recebido
-// via get através do formulário
-// de cadastro
+// via get através do formulário de cadastro
+
 $nome = $_GET["nome"];
 
-/// Insere o hábito na tabela
-// habito do banco de dados
+/// Insere o hábito na tabela habito do banco de dados
 
 
 $sql = " INSERT INTO habito (nome, status)
 VALUES ('".$nome."', 'A')";
 
 // Verifica se ocorreu tudo bem
-// Caso houve erro, fecha a conexão
-// e aborta o programa
+// Caso houver erro, fecha a conexão e aborta o programa
 
 if (!($conexao->query($sql) === TRUE)) {
     $conexao->close();
     die("Erro: " . $sql . "<br>" . $conexao->error);
    }
 
-// Fecha a conexão com o
-// Banco de dados
+// Fecha a conexão com o Banco de dados
 
 $conexao->close();
+
 // Envia para a página index
-// onde aparece a lista de hábitoss
-// já com o novo hábito cadastrado
 
 header("Location: index.php");
 ?>
