5d82641fbcfbd12306f23917bdf10d86f887decb
Kali <131703095+KaliRibeiro@users.noreply.github.com>
2024-12-12 17:31:27 -0300

    Update vencerhabito.php

--- vencerhabito.php
+++ vencerhabito.php
@@ -17,15 +17,13 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
    }
 
-// Atualiza o status de A - ativo
- // para V - vencido
+// Atualiza o status de A - ativo para V - vencido
 
  $id = $_GET["id"];
 $sql = " UPDATE habito "." SET status='V' " ." WHERE id=".$id;
 
 
- // Verifica se o comando foi
- // executado com sucesso
+ // Verifica se o comando foi executado com sucesso
 
  if (!($conn->query($sql) === TRUE)) {
     $conn->close();
