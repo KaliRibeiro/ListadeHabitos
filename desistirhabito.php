452d797927b4f5688a9aefdcc4b7483a59ef4f04
Kali <131703095+KaliRibeiro@users.noreply.github.com>
2024-12-12 17:46:27 -0300

    Update desistirhabito.php

--- desistirhabito.php
+++ desistirhabito.php
@@ -3,24 +3,25 @@ $servidor = "localhost";
 $usuario = "root";
 $senha = "";
 $bancodedados = "listadehabito";
+
 // Cria a conexão
+
 $conn = new mysqli( $servidor
  , $usuario
  , $senha
  , $bancodedados);
-// Verifica se conectou
-// Com sucesso
+
+// Verifica se conectou com sucesso
+
 if ($conn->connect_error) {
  die("A conexão falhou: " . $conn->connect_error);
 }
-// Obtém o id do registro
-// que foi recebido via get
+// Obtém o id do registro que foi recebido via get
 
 $id = $_GET["id"];
 $sql = "DELETE FROM habito WHERE id=".$id;
 
-// Executa o comando delete
-// da variável $sql
+// Executa o comando delete da variável $sql
 
 if (!($conn->query($sql) === TRUE)) {
  die("Erro ao excluir: " . $conn->error);
